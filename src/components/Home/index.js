e1dfaac702a24a1ddc2daa3c54704d76a20033b7
madhuchintha827 <chinthamadhu827@gmail.com>
2022-12-01 11:24:04 +0000

    solution

---
+++ src/components/Home/index.js
@@ -0,0 +1,39 @@
+// Write your code here
+import {Component} from 'react'
+
+import './index.css'
+
+import Login from '../Login'
+
+import Logout from '../Logout'
+
+import Message from '../Message'
+
+class Home extends Component {
+  state = {isLoggedIn: false}
+
+  onClickButton = () => {
+    this.setState(prevState => ({isLoggedIn: !prevState.isLoggedIn}))
+  }
+
+  render() {
+    const {isLoggedIn} = this.state
+    return (
+      <div className="container">
+        <div className="sub-container">
+          {isLoggedIn ? (
+            <Message message="Welcome User" />
+          ) : (
+            <Message message="Please Login" />
+          )}
+          {isLoggedIn ? (
+            <Logout logout={this.onClickButton} />
+          ) : (
+            <Login login={this.onClickButton} />
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+export default Home
